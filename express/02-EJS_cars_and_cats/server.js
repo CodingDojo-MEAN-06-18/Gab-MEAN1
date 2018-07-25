693b56c448208dac074a6789192c74ca8a112091
gabriela-v <gabcode@zoho.com>
2018-07-25 02:31:11 -0500

    Add files via upload

---
+++ express/02-EJS_cars_and_cats/server.js
@@ -0,0 +1,53 @@
+// Load the express module and store it in the variable express (Where do you think this comes from?)
+var express = require("express");
+console.log("Let's find out what express is", express);
+// invoke express and store the result in the variable app
+var app = express();
+console.log("Let's find out what app is", app);
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+app.get("/cars", function (request, response){
+  var cars_array = [
+    {name: "audi", img_source: "/images/audi-tt.jpg"}, 
+    {name: "bmw", img_source: "/images/bmw-x6.jpg"}, 
+    {name: "tesla-s", img_source: "/images/tesla-s.jpg"}, 
+    {name: "tesla-suv", img_source: "/images/tesla-suv.jpg"}, 
+  ];
+  response.render('cars', {cars: cars_array});
+})
+
+app.get("/cats", function (request, response){
+  var cats_array = [
+    {name: "cat1", img_source: "/images/cutecat1.jpg"}, 
+    {name: "cat2", img_source: "/images/cutecat2.jpg"}, 
+    {name: "cat3", img_source: "/images/cutecat3.jpg"}, 
+  ];
+  response.render('cats', {cats: cats_array});
+})
+
+app.get("/new", function (request, response){
+  response.render('new');
+})
+
+// use app's get method and pass it the base route '/' and a callback
+app.get('/', function(request, response) {
+    // just for fun, take a look at the request and response objects
+   console.log("The request object", request);
+   console.log("The response object", response);
+   // use the response object's .send() method to respond with an h1
+   response.send("<h1>Hello Express</h1>");
+})
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
